8f4ed4b6b3d784b56ba93d46eb22d603894ddbe3
Seth Bertlshofer <celestials2013@gmail.com>
2017-01-20 12:44:32 -0700

    separated render from update

--- gameLoop.js
+++ gameLoop.js
@@ -1,6 +1,6 @@
 var gameLoop = (()=>{
     'use strict';
-    
+
     var prevTime = 0;
     var events = [];
 
@@ -9,6 +9,7 @@ var gameLoop = (()=>{
         prevTime = time;
 
         update(elapsedTime);
+        render();
 
         window.requestAnimationFrame(gameLoop);
     }
@@ -16,27 +17,31 @@ var gameLoop = (()=>{
     function update(elapsedTime) {
         events.forEach((event, i)=>{
             event.time += elapsedTime;
+            event.isUpdate = false;
+
             if(event.time >= event.interval) {
                 if(event.repeat != 0) {
                     event.repeat -= 1;
                     event.time -= event.interval;
-                    render(event);
+                    event.isUpdate = true;
                 }
             }
             events[i] = event;
         });
 
         events = events.filter((item)=>{
-            return (item.repeat != 0) ? item : null; 
+            return (item.repeat != 0) ? item : null;
         });
     }
 
-    function render(event) {
-        if(event) {
-            let node = document.getElementById('output-section');
-            node.innerHTML += "Event: " + event.eventName + "(" + event.repeat + " remaining)\n";
-            node.scrollTop = node.scrollHeight;
-        }
+    function render() {
+        events.forEach((event, i)=>{
+            if(event.isUpdate && event.repeat != 0) {
+                let node = document.getElementById('output-section');
+                node.innerHTML += "Event: " + event.eventName + "(" + event.repeat + " remaining)\n";
+                node.scrollTop = node.scrollHeight;
+            }
+        });
     }
 
     function addEvent(){
@@ -44,7 +49,7 @@ var gameLoop = (()=>{
         let interval = parseInt(document.getElementById('interval').value);
         let repeat = parseInt(document.getElementById('repeat').value);
 
-        events.push({eventName, interval, repeat, time: 0});
+        events.push({eventName, interval, repeat, time: 0, isUpdate: false});
 
         document.getElementById('event-name').value = "";
         document.getElementById('interval').value = "";
